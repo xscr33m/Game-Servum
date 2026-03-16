e84a108ca2344c5b7bbcc49ab63b149073dfdf10
xscr33m <xscr33m@gmail.com>
2026-03-16 01:25:26 +0100

    feat: bump version to 0.9.7 across all packages

--- packages/shared/src/constants/index.ts
+++ packages/shared/src/constants/index.ts
@@ -3,7 +3,7 @@
  */
 
 /** Current application version (SemVer) */
-export const APP_VERSION = "0.9.6";
+export const APP_VERSION = "0.9.7";
 
 /** API version identifier */
 export const API_VERSION = "v1";
